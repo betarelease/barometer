1d5fdeaeb892ca3ac57db7f05f9828858829e6a2
Tyler Mercier <tylermercier@gmail.com>
2011-05-17 19:55:52 -0700

    create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barometer::Application.config.secret_token = '6e431ea26e9910f4ef9a56af9b1f08bbbf590f6b21905f4b1a33a71cfe80bee8e27abac91fedf4bd7990c1b8e23b31142f4259f6a902cad7f4cd6c861ae122a8'
